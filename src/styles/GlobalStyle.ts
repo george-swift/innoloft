ea3b2bd0d4d29fe29aee7d188f5d470bdf3845a0
George <71434803+george-swift@users.noreply.github.com>
2022-04-29 19:46:54 +0100

    Update global style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -79,6 +79,139 @@ const GlobalStyle = createGlobalStyle<MainThemeProps>`
   body {
     min-height: 100%;
   }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0 0 10px;
+    font-weight: 700;
+    color: ${theme.colors.heading};
+  }
+
+  h1 {
+    font-weight: 900;
+    font-size: 9vw;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  button {
+    background-color: inherit;
+    border: none;
+    cursor: pointer;
+    user-select: none;
+    transition: 0.3s;
+  }
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+
+  a {
+    display: inline-block;
+    text-decoration: none;
+    color: inherit;
+    transition: all 0.25s;
+    cursor: pointer;
+  }
+
+  p {
+    margin: 0 0 10px;
+  }
+
+  form {
+    .button-group {
+      text-align: right;
+    }
+
+    .field-group {
+      position: relative;
+      display: flex;
+      flex-direction: column-reverse;
+      margin-bottom: 1.5rem;
+  
+      label {
+        font-size: ${theme.fontSizes.xs};
+        margin-bottom: 0.5rem;
+        font-weight: 500;
+      }
+  
+      input,
+      select {
+        width: 100%;
+        padding: 0.5rem;
+        border: 1px solid ${theme.colors.gray};
+        caret-color: ${theme.colors.blue};
+        outline: 0;
+        transition: 0.2s border-color;
+  
+        &:placeholder-shown {
+          color: ${theme.colors.gray};
+        }
+  
+        &:focus {
+          border-color: ${theme.colors.blue};
+        }
+  
+        &:required + label:after {
+          content: '*';
+          font-size: 1.9em;
+          position: relative;
+          top: 6px;
+          display: inline-flex;
+          margin-left: 0.2ch;
+          transition: color 1s;
+        }
+  
+        &:required:invalid + label:after {
+          color: ${theme.colors.red};
+        }
+  
+        &:required:valid + label:after {
+          color: ${theme.colors.green};
+        }
+      }
+  
+      select {
+        width: 100%;
+        appearance: none;
+        background-color: ${theme.colors.smoke};
+        border: none;
+        border-radius: 0;
+        font-size: ${theme.fontSizes.sm};
+  
+        & + label {
+          font-size: ${theme.fontSizes.sm};
+        }
+      }
+
+      small {
+        margin-top: 1rem;
+        max-width: 300px;
+      }
+  
+      .arrow {
+        position: absolute;
+        content: '';
+        top: 36px;
+        right: 10px;
+        width: 0;
+        height: 0;
+        border: 6px solid transparent;
+        border-color: ${theme.colors.blue} transparent transparent
+          transparent;
+        pointer-events: none;
+      }
+    }
+
+    .field-group + button {
+      display: block;
+      margin-left: auto;
+    }
+  }
 `;
 
 export default GlobalStyle;
