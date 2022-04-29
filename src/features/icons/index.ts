0d3a717439fe3a52c02f4120179cf1ea9df70f80
George <71434803+george-swift@users.noreply.github.com>
2022-04-29 15:24:41 +0100

    Add and export icons relevant to style guide

---
+++ src/features/icons/index.ts
@@ -0,0 +1,11 @@
+export { default as IconAdd } from './add';
+
+export { default as IconCompany } from './company';
+
+export { default as IconEdit } from './edit';
+
+export { default as IconHome } from './home';
+
+export { default as IconInnoloft } from './innoloft';
+
+export { default as IconProduct } from './product';
