474ab4f535f795c59245aeaebdaf0e292b16f851
George <71434803+george-swift@users.noreply.github.com>
2022-04-29 19:49:27 +0100

    Add modal reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { configurationApi } from '../services/configuration';
 import { productApi } from '../services/product';
+import modalReducer from '../features/product/modalSlice';
 
 export const store = configureStore({
   reducer: {
     [configurationApi.reducerPath]: configurationApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
+    modal: modalReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(
